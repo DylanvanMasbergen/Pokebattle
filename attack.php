fdcfa2331612d9131f2e7b37914188e90f829c06
Unknown <99039655@mydavinci.nl>
2019-03-22 12:03:01 +0100

    Created Attack.php file

---
+++ attack.php
@@ -0,0 +1,13 @@
+<?php
+
+class Attack
+{
+	public $attackName;
+	public $attackDmg;
+
+	public function __construct($attackName,$attackDmg)
+	{
+		$this->attackName = $attackName;
+		$this->attackDmg = $attackDmg;
+	}
+}
