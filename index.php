c8b46ad94ee68f98df63ab74cc7d735264296994
Unknown <99039655@mydavinci.nl>
2019-03-29 11:01:55 +0100

    Changed showing the attack.

--- index.php
+++ index.php
@@ -15,42 +15,34 @@ $types = [
 ];
 
 //pokemon 1
-$pokemon1 = new pokemon(
-	"Cop",
+$pokemon1 = new Pikachu(
+	"Pikachu",
 	$types[0],
 	150,
-	new Resistance($types[2], 20),
-    new Weakness($types[1], 1.5),
+	new Resistance($types[2], 10),
+    new Weakness($types[3], 1.5),
 	[
-		new attack ('"Shot to the chest"', 100),
-		new attack ('"On tha Goddamn ground!"', 50)
+		new attack ('"Electric Ring"', 50),
+		new attack ('"Pika Punch"', 20)
 	]
 );
 
 //pokemon 2
-$pokemon2 = new pokemon(
-	"Gangsta",
-	$types[1],
-	150,	
-	new Resistance($types[3], 20),
-    new Weakness($types[1], 1.5),
+$pokemon2 = new Charmeleon(
+	"Charmeleon",
+	$types[2],
+	150,
+	new Resistance($types[3], 10),
+    new Weakness($types[0], 2),
 	[
-		new attack ('"Bust a cap in ur ass"', 100),
-		new attack ('"Pistol Whip"', 50)
+		new attack ('"Head Butt"', 10),
+		new attack ('"Flare"', 30)
 	]
 );
- 
- //Shows the attacks and the amount of damage dealt to the opponent.
-$result1 = $pokemon2->attack($pokemon2->attacks[1], $pokemon2, $pokemon1);
 
-	echo $pokemon2->name . " attacks " . $pokemon1->name . " with " . $pokemon2->attacks[1]->attackName . " and deals " . $result1[0] . "damage.";
-	echo "<br>";
-	echo $pokemon1->name . " has " . $result1[1] . " hitPoints of the " . $pokemon2->hitPoints . " left.";
+echo $pokemon1->attacks[0]->attack($pokemon1,$pokemon2);
 
 
-
-//prettyPrint($pokemon1);
-//prettyPrint($pokemon2);
 
 function prettyPrint($var)
 {
