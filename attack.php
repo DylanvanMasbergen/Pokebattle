d7a6ad5573a42e059a8e3a890733fb66f24ef0f9
Unknown <99039655@mydavinci.nl>
2019-03-29 10:54:57 +0100

    Changed the pokemon names and attacks.

--- attack.php
+++ attack.php
@@ -10,4 +10,23 @@ class Attack
 		$this->attackName = $attackName;
 		$this->attackDmg = $attackDmg;
 	}
+
+	public function attack($attacker, $defender)
+	{	
+		
+		if($attacker->type === $defender->weakness->name){
+            $attackDmg = $this->attackDmg * $defender->weakness->multiplier;
+            $attackMessage = $attacker->name.' attacked '.$defender->name.' and the attack was lethal, it did '.$attackDmg.'dmg';
+		}
+		else if ($attacker->type === $defender->resistance->name) {
+            $attackDmg = $this->attackDmg - $defender->resistance->resistance;
+            $attackMessage = $attacker->name. ' attacked '.$defender->name.' and the attack was non-lethal, it did '.$attackDmg.'dmg';
+		}
+		else{
+            $attackDmg = $this->attackDmg;
+            $attackMessage = $attacker->name.' attacked '.$defender->name.' and did '.$attackDmg.'dmg';
+        }
+        return($attackMessage);
+       	$defender->doDmg($defender, $attackDmg);
+	}
 }
